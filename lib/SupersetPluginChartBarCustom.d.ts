a898a36a7a0993b51d8f418fbf129c232ea00f58
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-03-20 09:16:42 +0700

    Add build

---
+++ lib/SupersetPluginChartBarCustom.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+import { SupersetPluginChartBarCustomProps } from './types';
+/**
+ * ******************* WHAT YOU CAN BUILD HERE *******************
+ *  In essence, a chart is given a few key ingredients to work with:
+ *  * Data: provided via `props.data`
+ *  * A DOM element
+ *  * FormData (your controls!) provided as props by transformProps.ts
+ */
+export default function SupersetPluginChartBarCustom(props: SupersetPluginChartBarCustomProps): JSX.Element;
+//# sourceMappingURL=SupersetPluginChartBarCustom.d.ts.map
